05cdc07d798008d4d023b048d4ed079884f81482
sitraka <hobynyaina.sitraka@gmail.com>
2025-02-20 08:28:36 +0300

    feat: ajout actions dans chaque items du tableau de la liste de produit

--- src/app/(protected)/products/_components/column.tsx
+++ src/app/(protected)/products/_components/column.tsx
@@ -2,6 +2,15 @@
 
 import { Avatar, Image } from "@radix-ui/react-avatar";
 import { ColumnDef } from "@tanstack/react-table"
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { MoreHorizontal } from "lucide-react";
 
 // This type is used to define the shape of our data.
 export interface IProduct {
@@ -16,7 +25,7 @@ export interface IProduct {
     count: number;
   };
 }
-  
+
 export const columns: ColumnDef<IProduct>[] = [
   {
     accessorKey: "title",
@@ -36,5 +45,31 @@ export const columns: ColumnDef<IProduct>[] = [
   {
     accessorKey: "price",
     header: "Prix",
-  }
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const product = row.original
+ 
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem
+              onClick={() => console.log(product)}
+            >Visualisation</DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem
+              onClick={() => console.log(product)}
+            >Suppression</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      )
+    },
+  },
 ]
--- src/app/(protected)/products/page.tsx
+++ src/app/(protected)/products/page.tsx
@@ -4,14 +4,20 @@ import { DataTable } from "./_components/data-table";
 import { columns } from "./_components/column";
 import { TableSkeleton } from "./_components/table-skeleton";
 import { useProducts } from "@/hooks/useProducts";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 
 const ProductsPage = () => {
 
   // query using tanstack query for getting products
   const { data, isLoading, isError } = useProducts();
 
+  const router = useRouter();
+
   return (
     <div className="container mx-auto p-10">
+      <h1 className="text-2xl font-bold mb-4">Products</h1>
+      <Button className="mb-4" onClick={() => router.push('/products/create')}>Créer un produit</Button>
       {isLoading && <TableSkeleton />}
       {isError && <p>Error</p>}
       {data && (
